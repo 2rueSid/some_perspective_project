1cbed985386e3254a23bef36973d188086d18811
2rueSid <mekaa2.718@gmail.com>
2021-07-29 18:23:00 +0300

    search by username

--- api/src/search/search.resolver.ts
+++ api/src/search/search.resolver.ts
@@ -1,7 +1,14 @@
-import { Query, Mutation, Resolver } from '@nestjs/graphql';
+import { Query, Mutation, Resolver, Args } from '@nestjs/graphql';
+import { User } from '@prisma/client';
+import { UserGraphQL } from 'src/user/user.dto';
 import { SearchService } from './search.service';
 
 @Resolver()
 export class SearchResolver {
   constructor(private readonly searchService: SearchService) {}
+
+  @Query(() => [UserGraphQL])
+  async searchByUserName(@Args('name') name: string): Promise<User[]> {
+    return await this.searchService.searchUserByName(name);
+  }
 }
