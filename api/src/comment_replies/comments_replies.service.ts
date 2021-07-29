7a8c8d2b55c7a5d23ad2ce733441d29808870171
2rueSid <mekaa2.718@gmail.com>
2021-07-29 16:12:33 +0300

    reply service

--- api/src/comment_replies/comments_replies.service.ts
+++ api/src/comment_replies/comments_replies.service.ts
@@ -4,4 +4,15 @@ import { PrismaService } from 'src/prisma_client/prisma.service';
 @Injectable()
 export class CommentRepliesService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createReply(commentId: number, replyTo: number): Promise<boolean> {
+    const res = await this.prisma.commentReplies.create({
+      data: {
+        comment_id: commentId,
+        reply_to: replyTo,
+      },
+    });
+
+    return !!res;
+  }
 }
