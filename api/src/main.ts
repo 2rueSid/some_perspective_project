e6a2d294d30db60da7658dd93f56e44dc17caaee
2rueSid <mekaa2.718@gmail.com>
2021-08-03 17:58:16 +0300

    init docker

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { PrismaService } from './prisma_client/prisma.service';
 import { AppModule } from './app.module';
@@ -8,9 +9,25 @@ async function bootstrap() {
     logger: ['warn', 'debug', 'verbose', 'log', 'error'],
   });
 
+  const user = process.env.RABBITMQ_USER;
+  const password = process.env.RABBITMQ_PASSWORD;
+  const host = process.env.RABBITMQ_HOST;
+  const queueName = process.env.RABBITMQ_QUEUE_NAME;
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://rabbitmq:5672`],
+      queue: queueName,
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
-  await app.listen(process.env.PORT);
+  await app.startAllMicroservices();
 }
 bootstrap();
